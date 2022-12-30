6637129e606179790756a2d456896909cd399878
Gasuhu <rtomio@live.fr>
2022-12-30 01:12:15 +0100

    yarn added

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-
-var fs = require('fs');
 const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3001;
+
+const port = process.env.PORT || 3001;
 server = express();
 server.use(express.static('public'))
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running `);
 });
